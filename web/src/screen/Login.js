70de6052d4c1c3478e531af2aab58c0882ec6e37
Manan <mananman23@gmail.com>
2022-07-31 09:47:54 +0530

    Camera module ready - will link with backend later

--- web/src/screen/Login.js
+++ web/src/screen/Login.js
@@ -1,45 +1,109 @@
-import React from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import '../styles/login.css';
 
 //? Components
 import Navbar from '../components/Navbar';
 import Btn from '../components/Btn';
 
+
 function Login() {
-  return (
-    <>
-        <Navbar />
+    const cameraRef = useRef(null);
 
-        <img src='./img/login.png' className='bg' />
+    const camera = function () {
+        let width = 0;
+        let height = 0;
 
-        <div className='container'>
-          <h1>Log In</h1>
-          <span>Log in with your data that you entered during registration</span>
+        const createObjects = function () {
 
-          <br />
-          <br />
 
-          <form action='/dashboard'>
-            <label>Email</label> <br/>
-            <input type='email' name='email' placeholder='E.g.- johndoe@gmail.com' required />
+            const video = document.createElement('video');
+            video.id = 'video';
+            video.width = width;
+            video.width = height;
+            video.autoplay = true;
+            cameraRef.current?.appendChild(video);
+            // setVideo(video);
 
-            <br/>
-            <br/>
+            const canvas = document.createElement('canvas');
+            canvas.id = 'canvas';
+            canvas.width = width;
+            canvas.width = height;
+            cameraRef.current?.appendChild(canvas);
+            // setCanvas(canvas);
+        }
+        return {
+            video: null,
+            context: null,
+            canvas: null,
 
-            <label>Password</label> <br/>
-            <input type='password' name='password' placeholder='Shhhhhhhhhh!!' required /> <br/>
-            <a href='#'>Forgot Password?</a>
+            startCamera: function (w = 680, h = 480) {
+                if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+                    width = w;
+                    height = h;
 
-            <br />
-            <br />
+                    createObjects();
 
-            <center>
-              <Btn text='Log In' />
-            </center>
-          </form>
-        </div>
-    </>
-  )
+                    this.video = document.getElementById('video');
+                    this.canvas = document.getElementById('canvas');
+                    this.context = this.canvas.getContext('2d');
+
+
+                    (function (video) {
+                        navigator.mediaDevices.getUserMedia({video: true}).then(function (stream) {
+                            video.srcObject = stream;
+                            video.play();
+                        });
+                    })(this.video)
+
+                }
+            },
+
+
+            takeSnapshot: function () {
+                this.context.drawImage(this.video, 0, 0, width, height);
+            }
+
+        }
+    }();
+    useEffect(() => {
+        camera.startCamera(320, 240);
+    }, []);
+
+    return (<>
+            <Navbar/>
+
+            <img src='./img/login.png' className='bg'/>
+
+            <div className='container'>
+                <h1>Log In</h1>
+                <span>Log in with your data that you entered during registration</span>
+
+                <br/>
+                <br/>
+
+                <form>
+                    <label>Email</label> <br/>
+                    <input type='email' name='email' placeholder='E.g.- johndoe@gmail.com' required/>
+
+                    <br/>
+                    <br/>
+
+                    <label>Iris Scanner</label> <br/>
+                    {/*<input type='password' name='password' placeholder='Shhhhhhhhhh!!' required /> <br/>*/}
+                    <center>
+                    <div className={"camera"} ref={cameraRef}></div>
+                    </center>
+                    <a href='#'>Can't login?</a>
+
+                    <br/>
+                    <br/>
+
+                    <center>
+                        <Btn text='Log In'/>
+                    </center>
+                </form>
+            </div>
+        </>)
 }
 
 export default Login
--- web/src/screen/Signup.js
+++ web/src/screen/Signup.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
 import '../styles/register.css';
 
 //? Components
@@ -6,48 +6,124 @@ import Navbar from '../components/Navbar';
 import Btn from '../components/Btn';
 
 function Signup() {
-  return (
-    <>
-        <Navbar />
 
-        <img src='./img/register.png' className='bgr' />
+    const cameraRef = useRef(null);
 
-        <div className='main'>
-            <div className='container'>
-            <h1>Sign Up</h1>
+    const camera = function () {
+        let width = 0;
+        let height = 0;
 
-            <br />
+        const createObjects = function () {
 
-            <form action='/dashboard'>
-              <label>Name</label> <br/>
-              <input type='text' name='name' placeholder='E.g.- John Doe' required />
 
-              <br/>
-              <br/>
+            const video = document.createElement('video');
+            video.id = 'video';
+            video.width = width;
+            video.width = height;
+            video.autoplay = true;
+            cameraRef.current?.appendChild(video);
+            // setVideo(video);
 
-              <label>Email</label> <br/>
-              <input type='email' name='email' placeholder='E.g.- johndoe@gmail.com' required />
+            const canvas = document.createElement('canvas');
+            canvas.id = 'canvas';
+            canvas.width = width;
+            canvas.width = height;
+            cameraRef.current?.appendChild(canvas);
+            // setCanvas(canvas);
+        }
+        return {
+            video: null,
+            context: null,
+            canvas: null,
 
-              <br/>
-              <br/>
+            startCamera: function (w = 680, h = 480) {
+                if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+                    width = w;
+                    height = h;
 
-              <label>Password</label> <br/>
-              <input type='password' name='password' placeholder='Shhhhhhhhhh!!' required />
+                    createObjects();
 
-              <br />
-              <br />
+                    this.video = document.getElementById('video');
+                    this.canvas = document.getElementById('canvas');
+                    this.context = this.canvas.getContext('2d');
 
-              <label>Confirm Password</label> <br/>
-              <input type='password' name='cpassword' placeholder='Shhhhhhhhhh!!' required />
 
-              <center>
-                  <Btn text='Sign Up' />
-              </center>
-            </form>
+                    (function (video) {
+                        navigator.mediaDevices.getUserMedia({video: true}).then(function (stream) {
+                            video.srcObject = stream;
+                            video.play();
+                        });
+                    })(this.video)
+
+                }
+            },
+
+
+            takeSnapshot: function () {
+                this.context.drawImage(this.video, 0, 0, width, height);
+            }
+
+        }
+    }();
+    useEffect(() => {
+        camera.startCamera(320, 240);
+    }, []);
+
+    return (
+        <>
+            <Navbar/>
+
+            <img src='./img/register.png' className='bgr'/>
+
+            <div className='main'>
+                <div className='container'>
+                    <h1>Sign Up</h1>
+
+                    <br/>
+
+                    <form action='/dashboard'>
+                        <label>Name</label> <br/>
+                        <input type='text' name='name' placeholder='E.g.- John Doe' required/>
+
+                        <br/>
+                        <br/>
+
+                        <label>Email</label> <br/>
+                        <input type='email' name='email' placeholder='E.g.- johndoe@gmail.com' required/>
+
+                        <br/>
+                        <br/>
+
+                        <label>Backup Password</label> <br/>
+                        <input type='password' name='password' placeholder='Shhhhhhhhhh!!' required/>
+
+                        <br/>
+                        <br/>
+
+                        <label>Confirm Password</label> <br/>
+                        <input type='password' name='cpassword' placeholder='Shhhhhhhhhh!!' required/>
+
+                        <br/>
+                        <br/>
+
+                        <label>Phone Number</label> <br/>
+                        <input type='text' name='phone' placeholder='E.g.- +639123456789' required/>
+
+                        <br/>
+                        <br/>
+
+                        <label>Iris Scanner</label> <br/>
+                        <center>
+                        <div className={"camera"} ref={cameraRef}></div>
+                        </center>
+                        <center>
+                            <Btn text='Sign Up'/>
+                        </center>
+                    </form>
+                </div>
             </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Signup
